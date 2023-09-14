6b96713e7f5fa2f9ba1520f86ad321eae9985e8e
GOKILA10 <gokisanthu12@gmail.com>
2023-09-14 10:35:34 +0530

    new code added

--- JS-Day4/Day4task.js
+++ JS-Day4/Day4task.js
@@ -2,21 +2,35 @@
 //for the given JSON iterate over  all loop(for in, for of, for each loop)
 
 
+//For Loop Condition
 
-const jsonObject = {
-    "name": "mary",
-    "age": 21,
-    "city": "netherland"
-  };
+let Fruitsname =[{"dragonfruit":"china","banana":"india","cherry":"ooty"}]
+ 
+  for(let i=0;i<Fruitsname.length;i++);
+  {
+    console.log(Fruitsname[i].dragonfruit);
+    console.log(Fruitsname[i].banana);
 
-  
-// Using a for...in loop
-
-
-for (let key in jsonObject) {
-    console.log(key + ": " + jsonObject[key]);
   }
 
+
+  // For in loop condition
+
+  let office =[{"company":"wati","unit":"HR","year":"2022"}]
+  for(let i in office)
+        {
+          console.log(i);
+        }
+
+
+  // For of loop condition
+  
+  let dress=[{"Brand":"firstcry","Model":"bodysuits", "price":"550"},
+  {"Brand":"Ed-ma-mama","Model":"quilts", "price":"2500"}]
+      for ( const best of dress)
+      {
+     console.log(dress);
+      }
 
 
 
